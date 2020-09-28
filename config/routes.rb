f6500e19b27b992ce0f1b3bb516a90434a6aac60
Jose Daniel Asuncion <josedanielasn@yahoo.com>
2020-09-28 19:37:19 +0800

    Added routes for create of purchases and interests / Added routes for show of pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   root  'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # For Pages
+  get "/category/:access_keyword" => 'pages#show', as: 'show_category'
+  
+  # For Interests
+  post "/category/:access_keyword/create/:symbol" => 'interests#create', as: 'create_interest'
+
+    # For Purchases
+  post "/category/:access_keyword/create/:symbol" => 'purchases#create', as: 'create_purchase'
 end
